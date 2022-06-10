32c7178ebf2ed2a70c114412bcf34adf96fac1b8
Cristian <cristianferrari95@gmail.com>
2022-06-10 23:05:11 +0200

    final

--- graph.py
+++ graph.py
@@ -1,12 +1,10 @@
-import graph
-
 class DeBruijnGraph:
 
     class Vertice:
     # Vertice del grafo
         def __init__(self, label):
             self.label = label         #kmer label
-            self.in_degree = 0
+            self.in_degree = 0         #num arco entranti e uscenti
             self.out_degree = 0
             
         def isSemiBalanced(self):
@@ -25,41 +23,49 @@ class DeBruijnGraph:
         def __str__(self):
             return self.label
 
-    class Arco:
 
-        def __init__(self, to_node):
-            # self.from_node = from_node
-            self.to_node = to_node
-            self.weight = 0
-
-
-    def __init__(self, kmers):    #TODO: is this even needed?
-        self.archi = {}      
-        self.vertici = {} 
+    def __init__(self, kmers): 
+        self.archi = {}                    #multi-mappa nodo-list(nodo)     
+        self.vertici = {}                  #mappa dei nodi
+        self.archi_og = {}
         self.head, self.tail = None, None        
 
         for kmers in kmers:
-            kmer_L, kmer_R = kmers[:-1], kmers[1:]                         #divido in k-1 mers
+            kmer_L, kmer_R = kmers[:-1], kmers[1:]                #divido in k-1 mers
             vertice_L, vertice_R = None, None
-            if kmer_L in self.vertici:                                        #per evitare vertici duplicati ne si controllo l'esistenza
+            if kmer_L in self.vertici:                                   #per evitare vertici duplicati ne si controllo l'esistenza
                 vertice_L = self.vertici[kmer_L]
             else:
-                vertice_L = self.vertici[kmer_L] = self.Vertice(kmer_L)      #maybe dumb this one down into two commands
+                vertice_L = self.vertici[kmer_L] = self.Vertice(kmer_L)    
 
             if kmer_R in self.vertici:          
                 vertice_R = self.vertici[kmer_R]
             else:
                 vertice_R = self.vertici[kmer_R] = self.Vertice(kmer_R)
 
-            vertice_L.out_degree += 1
-            vertice_R.in_degree += 1
-            # print("vertice_L: " + vertice_L.label + " in: " + str(vertice_L.in_degree) + " out: " + str(vertice_L.out_degree))
-            # print("vertice_R: " + vertice_R.label + " in: " + str(vertice_R.in_degree) + " out: " + str(vertice_R.out_degree))
-            # print(self.archi.setdefault(vertice_L, []))
-            self.archi.setdefault(vertice_L, []).append(vertice_R)  
-
+            if not (self.isDupe(vertice_L, vertice_R)):
+                vertice_L.out_degree += 1
+                vertice_R.in_degree += 1
             
+            self.archi.setdefault(vertice_L, []).append(vertice_R)
+
+        self.archi_og = self.archi.copy()         #Salvo mappa archi originale per disegno
+        c = 1
+        for node in iter(self.archi.keys()):
+            l = self.archi[node]
+            c += 1
+            dup = list(dict.fromkeys(l))
+            self.archi.update({node : dup})
+
         self.balanceCount()
+
+    def isDupe(self, verticeL, verticeR):
+        edges = self.archi.get(verticeL, [])
+        flag = False
+        for elem in edges:
+            if elem == verticeR:
+                flag = True
+        return flag      
  
     def balanceCount(self):
         self.num_semi = 0
@@ -76,43 +82,30 @@ class DeBruijnGraph:
                     self.head = vertice
                 self.num_semi += 1
             else:
-                
                 self.num_notBal += 1
 
 
     def eulerianPath(self):
-        """ Find and return Eulerian path or cycle (as appropriate) """
-        assert self.isEulerian()
-        g = self.archi
+        assert self.isEulerian()          #ADD EXCEPTION MAYBE TO EXPLAIN ERROR
         if self.hasEulerianPath():
-            g = g.copy()
             assert self.head is not None
             assert self.tail is not None
-            g.setdefault(self.tail, []).append(self.head)
-        # graph g has an Eulerian cycle
-        tour = []
-        src = iter(g.keys()).__next__()
-        # print(g)
-        def __visit(n):
-            while len(g[n]) > 0:
-                dst = g[n].pop()
-                # print(dst)
-                __visit(dst)
-            tour.append(n)
-        
-        __visit(src)
-        tour = tour[::-1][:-1]
+            self.archi.setdefault(self.tail, []).append(self.head)   #aggiungo ciclo euleriano per avere l'ultimo nodo nel cammino
+        result = []
+        src = iter(self.archi.keys()).__next__()
+        def visita(n):
+            while len(self.archi[n]) > 0:
+                dst = self.archi[n].pop()     #visita ricorsiva per ogni arco
+                visita(dst)
+            result.append(n)
+        visita(src)
+        result = result[::-1][:-1]
             
         if self.hasEulerianPath():
-            # Adjust node list so that it starts at head and ends at tail
-            sti = tour.index(self.head)
-            tour = tour[sti:] + tour[:sti]
-            print(tour)
+            sti = result.index(self.head)
+            result = result[sti:] + result[:sti]
+        return list(map(str, result))
         
-        # Return node list
-        return map(str, tour)
-            
-
         
     def isEulerian(self):
         return self.isBalanced() or self.isSemiBalanced()
@@ -131,33 +124,21 @@ class DeBruijnGraph:
         """ Return true iff graph has Eulerian cycle. """
         return self.num_notBal == 0 and self.num_semi == 0
 
-    def toDot(self, dotFh, weights=False):
-        """ Write dot representation to given filehandle.  If 'weights'
-            is true, label edges corresponding to distinct k-1-mers
-            with weights, instead of writing a separate edge for each
-            copy of a k-1-mer. """
-        dotFh.write("digraph \"Graph\" {\n")
-        dotFh.write("  bgcolor=\"transparent\";\n")
-        # for node in iter(self.archi.keys()):
-        #     lab = node.label
-        #     dotFh.write("  %s [label=\"%s\"] ;\n" % (lab, lab))
-        for src, dsts in iter(self.archi.items()):
+    def printGraph(self, output, weights=False):
+        """ Write graph in dot represantion so that it
+            might be dsiplayed using graphviz"""
+        output.write("digraph \"DeBruijn Graph\" {\n")
+        output.write("  bgcolor=\"white\";\n")
+        for src, dsts in iter(self.archi_og.items()):
             # print(dsts)
             srclab = src.label
-            if weights:
-                weightmap = {}
-                if weights:
-                    for dst in dsts:
-                        weightmap[dst] = weightmap.get(dst, 0) + 1
-                for dst, v in iter(weightmap.items()):
-                    dstlab = dst.label
-                    dotFh.write("  %s -> %s [label=\"%d\"] ;\n" % (srclab, dstlab, v))
-            else:
-                for dst in dsts:
-                    srclab = src.label
-                    dstlab = dst.label
-                    dotFh.write("  %s -> %s [label=\"\"] ;\n" % (srclab, dstlab))
-        dotFh.write("}\n")
+            weightmap = {}
+            for dst in dsts:
+                weightmap[dst] = weightmap.get(dst, 0) + 1
+            for dst, v in iter(weightmap.items()):
+                dstlab = dst.label
+                output.write("  %s -> %s [label=\"%d\"] ;\n" % (srclab, dstlab, v))
+        output.write("}\n")
 
 
 
--- main.py
+++ main.py
@@ -1,32 +1,28 @@
+from asyncore import write
+from sysconfig import get_path
 from Bio import SeqIO
 from Bio.Seq import Seq
 from pathlib import Path
+from graphviz import Digraph as dg
+
 import utils
-import graphtest
 import graph
 
-path = Path(__file__) / "../fa-test.fa"   
+path = Path(__file__) / "../input.fa"   
 
 read = utils.read_input(path)
-# print(read)
-kmer = utils.splice(utils.read_input(path), 3)
-# print(kmer)
-# print(kmer)
-# print(list(dict.fromkeys(kmer)))
+kmer = utils.splice(utils.read_input(path), 5)
 
 
 db = graph.DeBruijnGraph(kmer)
-# db = graph.DeBruijnGraph(list(dict.fromkeys(kmer)))
-# print(db.hasEulerianPath())
-# print(db.eulerianPath())
-# print(dir(iter(db.G)))
 
-handle = open('graph.txt', 'w')
-db.toDot(handle, weights=True)
+handle = open('graph.gv', 'w')
+db.printGraph(handle)
+# g_path = Path('graph.gv')
+# dg.render(g_path)
 
-
-#Superstring assembly function :TODO put in separate method (maybe fuse path method with this one directly)
-kmer_list = list(db.eulerianPath())
+#Superstring assembly function
+kmer_list = db.eulerianPath()
 ss = kmer_list[0]
 i = 1
 while i < (len(kmer_list)):
@@ -34,9 +30,11 @@ while i < (len(kmer_list)):
     ss = ss + x[-1]
     i += 1
 
-print(ss)
+#print superstringa finale
+output = open('output.txt', "w")
+output.write(ss)
+output.close()
  
-#TODO: check duplicati maybe per le read multiple non funzionanti
 
 
 
